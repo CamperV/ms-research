471fa012d6b225a5cc1c51408b8390d0c4237064
Jay Danner <jdanner3@gatech.edu>
2016-03-12 14:26:53 -0500

    added step through functionality

--- main.cpp
+++ main.cpp
@@ -7,19 +7,48 @@
 // Free Software Foundation, either version 3 of the License
 // or (at your option) any later version.
 // (see http://www.opensource.org/licenses for more info)
+//
+// ---
+// Jay Danner
+// ---
 
-#include <highgui.h>
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/video/video.hpp"
+#include <opencv2/video/background_segm.hpp>
+
 #include "ChromacityShadRem.h"
 #include "GeometryShadRem.h"
 #include "LrTextureShadRem.h"
 #include "PhysicalShadRem.h"
 #include "SrTextureShadRem.h"
+#include "utils/VideoStats.h"
 
-int main() {
-	// load frame, background and foreground
-	cv::Mat frame = cv::imread("samples/frame.bmp");
-	cv::Mat bg = cv::imread("samples/bg.bmp");
-	cv::Mat fg = cv::imread("samples/fg.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+  
+  if(argc > 1) {
+    cout << "Opening video capture " << argv[1] << "..." << endl;
+  } else {
+    cout << "Enter video filename." << endl;
+    exit(0);
+  }
+
+  VideoCapture capture(argv[1]);
+  VideoStats vstats;
+
+  vstats.setWidth(capture.get(CV_CAP_PROP_FRAME_WIDTH));
+  vstats.setHeight(capture.get(CV_CAP_PROP_FRAME_HEIGHT));
+
+  if (!capture.isOpened()) { 
+    cout << "Capture failed to open." << endl; 
+    return -1; 
+  }
+
+  Mat frame, fg, bg;
+  capture >> frame;
 
 	// create shadow removers
 	ChromacityShadRem chr;
@@ -29,26 +58,56 @@ int main() {
 	LrTextureShadRem lrTex;
 
 	// matrices to store the masks after shadow removal
-	cv::Mat chrMask, phyMask, geoMask, srTexMask, lrTexMask;
+	Mat chrMask, phyMask, geoMask, srTexMask, lrTexMask;
 
-	// remove shadows
-	chr.removeShadows(frame, fg, bg, chrMask);
-	phy.removeShadows(frame, fg, bg, phyMask);
-	geo.removeShadows(frame, fg, bg, geoMask);
-	srTex.removeShadows(frame, fg, bg, srTexMask);
-	lrTex.removeShadows(frame, fg, bg, lrTexMask);
+  BackgroundSubtractorMOG2 MOG = BackgroundSubtractorMOG2();
+  MOG.set("detectShadows", false);
+  MOG.set("nmixtures", 3);
 
-	// show results
-	cv::imshow("frame", frame);
-	cv::imshow("bg", bg);
-	cv::imshow("fg", fg);
-	cv::imshow("chr", chrMask);
-	cv::imshow("phy", phyMask);
-	cv::imshow("geo", geoMask);
-	cv::imshow("srTex", srTexMask);
-	cv::imshow("lrTex", lrTexMask);
+  bool step = false;
+  // processing loop
+  for(;;) {
 
-	cv::waitKey();
+    /* PRE-PROCESSING */
 
+    capture >> frame;
+
+    // check if we need to restart the video
+    if(frame.empty()) {
+        // Looks like we've hit the end of our feed! Restart
+        cout << "Frame is empty, restarting..." << endl;
+        capture.set(CV_CAP_PROP_POS_AVI_RATIO, 0.0);
+        cout << "Done!" << endl;
+        continue;
+    }
+
+    MOG(frame, fg);
+    MOG.getBackgroundImage(bg);
+
+    /* PROCESSING */
+
+	  chr.removeShadows(frame, fg, bg, chrMask);
+	  //phy.removeShadows(frame, fg, bg, phyMask);
+	  //geo.removeShadows(frame, fg, bg, geoMask);
+	  //srTex.removeShadows(frame, fg, bg, srTexMask);
+	  lrTex.removeShadows(frame, fg, bg, lrTexMask);
+
+    /* POST-PROCESSING */
+
+    vstats.updateFPS();
+    vstats.displayStats();
+
+	  // show results
+	  imshow("frame", frame);
+	  imshow("fg", fg);
+	  imshow("chrMask", chrMask);
+	  imshow("lrTexMask", lrTexMask);
+
+    if(step) {
+      if(waitKey(0) == 115) step = !step;
+    } else {
+      if(waitKey(30) == 115) step = !step;
+    }
+  }
 	return 0;
 }
