d88fbcda0f0dceebdf9b4625b3b306e3b30f4483
Jay Danner <jdanner3@gatech.edu>
2017-01-31 17:09:34 -0500

    param_quantitative.py now creates 3D graphs

--- shadowdetection/param_quantitative.py
+++ shadowdetection/param_quantitative.py
@@ -3,26 +3,28 @@
 
 import sys
 import subprocess
-import matplotlib
 import os
 try:
     from configparser import ConfigParser
 except ImportError:
     from ConfigParser import ConfigParser  # ver. < 3.0
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
+# range for floats
 def frange(x, y, jump=1.0):
-  '''Range for floats.'''
   i = 0.0
   x = float(x)  # Prevent yielding integers.
   x0 = x
   epsilon = jump / 2.0
   yield x  # yield always first value
-
   while x + epsilon < y:
     i += 1.0
     x = x0 + i * jump
     yield x
 
+# take in args
 try:
   indir = sys.argv[1]
 
@@ -38,6 +40,7 @@ except:
   print "ex: python param_quantitative.py ./samples/PETs1 P,coneAngle 3.5,0.1"
   sys.exit(0)
 
+# build paths
 frames_path = indir + "/frames/"
 bgs_path = indir + "/bgs/"
 shadows_path = indir + "/shadows/"
@@ -57,6 +60,7 @@ else:
   print "Please enter valid sectionname."
   exit(0)
 
+# check if param is present/what is default
 # WITHOUT ConfigParser
 for line in open('default.ini'):
   if param in line:
@@ -65,26 +69,32 @@ for line in open('default.ini'):
     print "Init val of " + param + ": " + str(param_val)
 
 # ConfigParser impl
-# ---
 config = ConfigParser()
 config.optionxform = str  # make sure to not strip capitals
 config.read('default.ini')
 
-# read values from a section
-#param_val = config.getfloat(psection, param)
+xaxis = []
+yaxis = list(frange(0, float(upperrange), float(step)))
+zaxis = []
 
+print "Processing..."
+
+# processing
 listing = os.listdir(frames_path)
 for infile in sorted(listing):
   fr = frames_path+infile
   bg = bgs_path+infile
   sh = shadows_path+infile
 
+  # store filename (x-axis)
+  xaxis.append(int(infile.split('.')[0]))
+
   # for each param_val in range 0-upperrange
   for val in frange(0, float(upperrange), float(step)):
-    print val
+
     # update existing value
     config.set(psection, param, val)
-    
+  
     # save to a file
     with open('params.ini', 'w') as configfile:
         config.write(configfile)
@@ -95,27 +105,21 @@ for infile in sorted(listing):
     for line in process.stderr:
       splitline = line.strip("\n").split(",")
   
-      print splitline[0] + " " + splitline[1] + " " + splitline[2]
-        
-      # store filename (x-axis)
-      # store detection rate (y-axis)
-      # store discrimination rate (y-axis2)
-      # store changing param (z-axis)
-      # plot correlated variable (y-axis3)
-  
-      #if(splitline[0] == "C"):
-      #  cDetect += float(splitline[1])
-      #  cDiscrim += float(splitline[2])
-      #elif(splitline[0] == "P"):
-      #  pDetect += float(splitline[1])
-      #  pDiscrim += float(splitline[2])
-      #elif(splitline[0] == "G"):
-      #  gDetect += float(splitline[1])
-      #  gDiscrim += float(splitline[2])
-      #elif(splitline[0] == "SR"):
-      #  srDetect += float(splitline[1])
-      #  srDiscrim += float(splitline[2])
-      #elif(splitline[0] == "LR"):
-      #  lrDetect += float(splitline[1])
-      #  lrDiscrim += float(splitline[2])
-  
+      if(splitline[0] == section):
+        zaxis.append(float(splitline[1])) # detection
+
+# plot here
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+xs = np.array(xaxis)
+ys = np.array(yaxis)
+zs = np.array(zaxis).reshape(ys.size,xs.size)
+
+xs, ys = np.meshgrid(xs, ys)
+
+# surface graph
+ax.plot_surface(xs, ys, zs)
+
+plt.show()
+      
