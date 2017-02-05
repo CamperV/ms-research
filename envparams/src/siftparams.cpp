77910dd54f2f35ddc129efa991351071c6d0ee0b
Jay Danner <jdanner3@gatech.edu>
2017-02-05 15:51:39 -0500

    updated sift params to include 0.0-0.03 contrast ratios

--- envparams/src/siftparams.cpp
+++ envparams/src/siftparams.cpp
@@ -36,32 +36,59 @@ int main(int argc, char** argv) {
 
   // start SIFT analysis
   int numSIFT;
-  int numSIFT_lowc;
-  SiftFeatureDetector detector;
-  SiftFeatureDetector detector_lowc(0,3,0.01,10,1.6);
+  int numSIFT_lowc0, numSIFT_lowc1, numSIFT_lowc2, numSIFT_lowc3;
+
+  // default is 0.04 for contrast threshold
+  SiftFeatureDetector detector;     
+  SiftFeatureDetector detector_lowc0(0,3,0.00,10,1.6);
+  SiftFeatureDetector detector_lowc1(0,3,0.01,10,1.6);
+  SiftFeatureDetector detector_lowc2(0,3,0.02,10,1.6);
+  SiftFeatureDetector detector_lowc3(0,3,0.03,10,1.6);
 
   vector<KeyPoint> keypoints;
-  detector.detect(img, keypoints);
-
-  vector<KeyPoint> keypoints_lowc;
-  detector_lowc.detect(img, keypoints_lowc);
-
   Mat descriptors;
+  ///
+  detector.detect(img, keypoints);
   detector.compute(img, keypoints, descriptors);
-
+  ///
   numSIFT = keypoints.size();
 
+  vector<KeyPoint> keypoints_lowc;
   Mat descriptors_lowc;
-  detector_lowc.compute(img, keypoints_lowc, descriptors_lowc);
+  ///
+  detector_lowc0.detect(img, keypoints_lowc);
+  detector_lowc0.compute(img, keypoints_lowc, descriptors_lowc);
+  ///
+  numSIFT_lowc0 = keypoints_lowc.size();
 
-  numSIFT_lowc = keypoints_lowc.size();
+  ///
+  detector_lowc1.detect(img, keypoints_lowc);
+  detector_lowc1.compute(img, keypoints_lowc, descriptors_lowc);
+  ///
+  numSIFT_lowc1 = keypoints_lowc.size();
+
+  ///
+  detector_lowc2.detect(img, keypoints_lowc);
+  detector_lowc2.compute(img, keypoints_lowc, descriptors_lowc);
+  ///
+  numSIFT_lowc2 = keypoints_lowc.size();
+
+  ///
+  detector_lowc3.detect(img, keypoints_lowc);
+  detector_lowc3.compute(img, keypoints_lowc, descriptors_lowc);
+  ///
+  numSIFT_lowc3 = keypoints_lowc.size();
 
 #ifdef DEBUG
   cout << "Number of SIFT features: " << numSIFT << endl;
 #endif
 #ifndef DEBUG
-  cout << numSIFT << "," << numSIFT_lowc << "," 
-       << numSIFT_lowc-numSIFT << "," << (float)numSIFT/(float)numSIFT_lowc << endl;
+  cout << numSIFT << ","
+       << 1-((float)numSIFT/(float)numSIFT_lowc0) << "," 
+       << 1-((float)numSIFT/(float)numSIFT_lowc1) << "," 
+       << 1-((float)numSIFT/(float)numSIFT_lowc2) << "," 
+       << 1-((float)numSIFT/(float)numSIFT_lowc3)
+       << endl;
 #endif
 	return 0;
 }
